638d974f11a637f9dcfef886a944ee70f9489e40
aslawny <alexis.slawny@gmail.com>
2021-02-07 21:39:14 +0100

    db branchée

---
+++ src/firebaseDb.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAOsxrNK4SRyWh0OQ268O3qIttkUQ5m49Q",
+    authDomain: "immo-89c1d.firebaseapp.com",
+    projectId: "immo-89c1d",
+    storageBucket: "immo-89c1d.appspot.com",
+    messagingSenderId: "395902372438",
+    appId: "1:395902372438:web:b36a5a601b822611c8c9f7",
+    measurementId: "G-THZ51XTCCN"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebaseApp.firestore();
