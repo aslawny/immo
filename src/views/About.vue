fc76acaf6664d906ff858dccde660feaa6db9ab3
aslawny <alexis.slawny@gmail.com>
2021-02-09 22:45:56 +0100

    page à propos

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,48 @@
 <template>
   <div class="home">
     <div class="container">
-      <br />"Rien de grand ne s'est accomplis sans passion - Hegel<br /><br />
+      <br />
       <div class="home-img">
-        <router-link to="/contact"><img src="../assets/romain.jpg" /></router-link>
+        <h4>
+          Bonjour, je suis Romain TARDIF, mandataire immobilier indépendant rattaché au
+          réseau iad France.
+        </h4>
+        <br />
+        <table>
+          <tr>
+            <td>
+              <router-link to="/contact"><img src="../assets/romain.jpg" /></router-link>
+            </td>
+            <td>
+              J’occupe à ce titre la position de conseiller immobilier IAD sur la ville de
+              MARLY-LE-ROI. J’ai appris à connaître parfaitement ce secteur et ses
+              spécificités.<br />
+              À l’écoute de vos besoins, je vous apporte mon aide dans le cadre de votre
+              recherche d’un nouveau bien. En faisant appel à moi, vous gagnerez en temps,
+              en pertinence et en précision. <br />
+              Pour cela, je mets à votre service mon réseau et mon expérience de
+              conseiller immobilier dans le périmètre géographique où vous voulez vous
+              installer. Prix du mètre carré, emplacement, location ou vente, type de
+              bien, superficie recherchée à MARLY-LE-ROI sont des informations
+              essentielles pour réaliser votre future transaction immobilière. Elles vous
+              aideront à délimiter de façon claire votre projet immobilier.<br />
+              Mon rôle de mandataire immobilier à MARLY-LE-ROI me permet de vous
+              accompagner méthodiquement,à chaque étape du processus selon votre
+              convenance. À terme, vous aurez en votre possession tous les éléments pour
+              vous aider à réaliser votre transaction immobilière en parfaite
+              transparence. D’autre part, mon statut de mandataire immobilier indépendant
+              vous apporte d’autres garanties.<br />
+              En premier lieu, ma flexibilité dans la gestion de mes projets et la prise
+              de rendez-vous clients. Je peux donc me rendre aisément disponible sur
+              MARLY-LE-ROI et ses environs et être à vos côtés pendant toutes les étapes
+              de votre projet immobilier.Vente ou Achat de biens à MARLY-LE-ROI, je me
+              ferai le plaisir de vous conseiller sur tous les aspects pratiques liés à
+              votre projet.<br />
+              Nous nous sommes engagés à respecter un cadre réglementé, ce qui augmente
+              notre crédibilité et garantit la qualité de nos prestations.
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
   </div>
