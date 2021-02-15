ea4b0eb9d522e33c4e2341b5bfed67a5c2c6b85d
aslawny <alexis.slawny@gmail.com>
2021-02-15 21:53:50 +0100

    mise en forme annonce

--- src/views/AddAnnonces.vue
+++ src/views/AddAnnonces.vue
@@ -3,102 +3,118 @@
     <div class="container">
       <br />
       <div class="home-img">
-        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <form id="annonce_form" v-on:submit.prevent="addAnnonce">
-          <div class="form-group">
-            <label for="annonceType">Achat / Vente</label>
-            <select class="form-control" id="annonceType" v-model="annonce.av" required>
-              <option>Achat</option>
-              <option>Vente</option>
-            </select>
+          <div class="form-row">
+            <div class="col-md-4 mb-1">
+              <label for="annonceType">Achat / Vente</label>
+              <select class="form-control" id="annonceType" v-model="annonce.av" required>
+                <option>Achat</option>
+                <option>Vente</option>
+              </select>
+            </div>
+            <div class="col-md-4 mb-1">
+              <label for="annonceMv">Maison / Appt</label>
+              <select
+                class="form-control"
+                id="annonceMv"
+                v-model="annonce.maison"
+                required
+              >
+                <option>Maison</option>
+                <option>Appartement</option>
+              </select>
+            </div>
+            <div class="col-md-4 mb-1">
+              <label>Prix</label>
+              <input class="form-control" id="prix" v-model="annonce.prix" required />
+            </div>
+            <div class="col-md-4 mb-2">
+              <label>Ville</label>
+              <input class="form-control" id="ville" v-model="annonce.ville" required />
+            </div>
+            <div class="col-md-4 mb-2">
+              <label>CP</label>
+              <input class="form-control" id="cp" v-model="annonce.cp" required />
+            </div>
+            <div class="col-md-4 mb-2">
+              <label>Surface</label>
+              <input
+                class="form-control"
+                id="surface"
+                v-model="annonce.surface"
+                required
+              />
+            </div>
+            <div class="col-md-4 mb-3">
+              <label>Pieces</label>
+              <input class="form-control" id="pieces" v-model="annonce.pieces" required />
+            </div>
+            <div class="col-md-4 mb-3">
+              <label>Chambres</label>
+              <input
+                class="form-control"
+                id="chambres"
+                v-model="annonce.chambres"
+                required
+              />
+            </div>
+            <div class="col-md-4 mb-3">
+              <label>Année</label>
+              <input class="form-control" id="annee" v-model="annonce.annee" required />
+            </div>
+            <div class="col-md-6 mb-4">
+              <label for="contactComment">Descriptif :</label>
+              <textarea
+                class="form-control"
+                id="contactComment"
+                rows="3"
+                v-model="annonce.commentaire"
+                required
+              ></textarea>
+            </div>
+            <div class="col-md-6 mb-4">
+              <label for="equipement">Equipement :</label>
+              <textarea
+                class="form-control"
+                id="equipement"
+                rows="3"
+                v-model="annonce.equipement"
+                required
+              ></textarea>
+            </div>
+            <div class="col-md-4 mb-5">
+              <label for="energie">Energie</label>
+              <select
+                class="form-control"
+                id="energie"
+                v-model="annonce.energie"
+                required
+              >
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+                <option>D</option>
+                <option>E</option>
+                <option>F</option>
+                <option>G</option>
+              </select>
+            </div>
+            <div class="col-md-4 mb-5">
+              <label for="ges">Ges</label>
+              <select class="form-control" id="ges" v-model="annonce.ges" required>
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+                <option>D</option>
+                <option>E</option>
+                <option>F</option>
+                <option>G</option>
+              </select>
+            </div>
+            <button type="submit" class="btn btn-primary col-md-12 mb-5">Ajouter</button>
+            <br />
+            <br />
           </div>
-          <div class="form-group">
-            <label>Ville</label>
-            <input class="form-control" id="ville" v-model="annonce.ville" required />
-          </div>
-          <div class="form-group">
-            <label>CP</label>
-            <input class="form-control" id="cp" v-model="annonce.cp" required />
-          </div>
-          <div class="form-group">
-            <label>Prix</label>
-            <input class="form-control" id="prix" v-model="annonce.prix" required />
-          </div>
-          <div class="form-group">
-            <label for="annonceMv">Maison / Appt</label>
-            <select class="form-control" id="annonceMv" v-model="annonce.maison" required>
-              <option>Maison</option>
-              <option>Appartement</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label>Surface</label>
-            <input class="form-control" id="surface" v-model="annonce.surface" required />
-          </div>
-          <div class="form-group">
-            <label>Pieces</label>
-            <input class="form-control" id="pieces" v-model="annonce.pieces" required />
-          </div>
-          <div class="form-group">
-            <label>Chambres</label>
-            <input
-              class="form-control"
-              id="chambres"
-              v-model="annonce.chambres"
-              required
-            />
-          </div>
-          <div class="form-group">
-            <label>Année</label>
-            <input class="form-control" id="annee" v-model="annonce.annee" required />
-          </div>
-          <div class="form-group">
-            <label for="contactComment">Descriptif :</label>
-            <textarea
-              class="form-control"
-              id="contactComment"
-              rows="3"
-              v-model="annonce.commentaire"
-              required
-            ></textarea>
-          </div>
-          <div class="form-group">
-            <label for="equipement">Equipement :</label>
-            <textarea
-              class="form-control"
-              id="equipement"
-              rows="3"
-              v-model="annonce.equipement"
-              required
-            ></textarea>
-          </div>
-          <div class="form-group">
-            <label for="energie">Energie</label>
-            <select class="form-control" id="energie" v-model="annonce.energie" required>
-              <option>A</option>
-              <option>B</option>
-              <option>C</option>
-              <option>D</option>
-              <option>E</option>
-              <option>F</option>
-              <option>G</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="ges">Ges</label>
-            <select class="form-control" id="ges" v-model="annonce.ges" required>
-              <option>A</option>
-              <option>B</option>
-              <option>C</option>
-              <option>D</option>
-              <option>E</option>
-              <option>F</option>
-              <option>G</option>
-            </select>
-          </div>
-          <button type="submit" class="btn btn-primary">Ajouter</button>
-          <br />
-          <br />
         </form>
       </div>
     </div>
